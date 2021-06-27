14470db9d4f33170b07a2e8f889d7bc78a4e4709
k-gn <rlarbska97@gmail.com>
2021-06-27 23:49:26 +0900

    reactive stream

--- study01/src/main/java/com/cos/fluxtest/MyFilter.java
+++ study01/src/main/java/com/cos/fluxtest/MyFilter.java
@@ -1,12 +1,57 @@
 package com.cos.fluxtest;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter implements Filter {
-    
+
+    private EventNotify eventNotify;
+
+    public MyFilter(EventNotify eventNotify) {
+        this.eventNotify = eventNotify;
+    }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("필터 실행됨");    
+        System.out.println("필터 실행됨");
+
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        resp.setContentType("text/event-stream; charset=utf-8");
+        // 일반적인 마임타입은 flush 해도 읽지 않고 쌓아놓고 한번에 내보낸다.
+        // event-stream 타입은 flush 할때마다 내보낸다.
+        PrintWriter out = resp.getWriter();
+
+        // Reactive Streams 라이브러리로 표준을 지켜서 응답할 수 있다.
+        for(int i=0; i<5; i++) {
+            out.println("응답 : " + i);
+            out.flush();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // SSE Emitter 라이브러리를 쓰면 편하게 쓸 수 있다.
+        while(true) {
+            try {
+                if(eventNotify.isChange()) {
+                    System.out.println("change event");
+                    int lastIndex = eventNotify.getEvents().size() - 1;
+                    out.println("응답 : " + eventNotify.getEvents().get(lastIndex));
+                    out.flush();
+                    eventNotify.setChange(false);
+                }
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+    // WebFlux -> Reactive Streams 가 적용된 Stream 인데 비동기 단일 스레드 동작
+    // Servlet MVC -> Reactive Streams 가 적용된 Stream 인데 멀티 스레드 동작
 }
--- study01/src/main/java/com/cos/fluxtest/MyFilterConfig.java
+++ study01/src/main/java/com/cos/fluxtest/MyFilterConfig.java
@@ -1,5 +1,7 @@
 package com.cos.fluxtest;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,13 +9,23 @@ import org.springframework.context.annotation.Configuration;
 import javax.servlet.Filter;
 
 @Configuration
+@RequiredArgsConstructor
 public class MyFilterConfig {
+
+    private final EventNotify eventNotify;
 
     // 필터 등록하기
     @Bean
     public FilterRegistrationBean<Filter> addFilter() {
-        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(new MyFilter());
-        bean.addUrlPatterns("/*");
+        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(new MyFilter(eventNotify));
+        bean.addUrlPatterns("/sse");
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> addFilter2() {
+        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(new MyFilter2(eventNotify));
+        bean.addUrlPatterns("/add");
         return bean;
     }
 }
